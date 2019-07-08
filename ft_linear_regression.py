b3dd97abd4bb9f38df6ec6edb847125030024290
unknown <javigner@student.42.fr>
2019-07-08 20:54:31 +0200

    Visualisation

---
+++ ft_linear_regression.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importer le dataset
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Diviser le dataset entre le Training set et le Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1.0/3, random_state = 0)
+
+# Construction du modèle
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Faire de nouvelles prédictions
+y_pred = regressor.predict(X_test)
+regressor.predict(15)
+
+# Visualiser les résultats
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Prix voiture')
+plt.xlabel('kilometre')
+plt.ylabel('Prix')
+plt.show()
