acacb132f3d288489affd1bbe4149ee9631afed4
unknown <javigner@student.42.fr>
2019-07-14 20:13:27 +0200

    Gradient descent

--- Training.py
+++ Training.py
@@ -1,7 +1,6 @@
 # Importer les librairies
 import matplotlib.pyplot as plt
 import pandas as pd
-from math import pow
 
 # Importer le dataset
 dataset = pd.read_csv('Data.csv')
@@ -15,10 +14,17 @@ lentest = len(X)
 def prediction(theta0, theta1, test_value):
     return (theta0 + theta1 * test_value)
 
-def cost_function(theta0, theta1, X, y):
+def cost_function_theta0(theta0, theta1, X, y):
     result = 0
     for i in range(len(X)):
-        result = float(result + pow((theta0 + theta1 * X[i] - y[i]), 2))
+        result = float(result + (theta0 + theta1 * X[i] - y[i]))
+    costf = (1 / (2 * len(X))) * result
+    return costf
+
+def cost_function_theta1(theta0, theta1, X, y):
+    result = 0
+    for i in range(len(X)):
+        result = float(result + (theta0 + theta1 * X[i] - y[i]) * X[i])
     costf = (1 / (2 * len(X))) * result
     return costf
 
@@ -29,13 +35,13 @@ def Gradient_descent(theta0, theta1, X, y):
     while (theta0 != tmptheta0 & theta1 != tmptheta1):
         theta0 = tmptheta0
         theta1 = tmptheta1
-        tmptheta0 = theta0 - learning_rate * [] * cost_function(theta0, theta1, X, y)
-        tmptheta1 = theta1 - learning_rate * [] * cost_function(theta0, theta1, X, y)
+        tmptheta0 = theta0 - learning_rate * (1 / len(X)) * cost_function_theta0(theta0, theta1, X, y)
+        tmptheta1 = theta1 - learning_rate * (1 / len(X)) * cost_function_theta1(theta0, theta1, X, y) 
     return (theta0, theta1)
     
-def linear_regression(X, y, theta0, theta1, x_test):
-    pred = theta0 + theta1 * x_test
-    somme = sum(X)
+def main():
     
- print(cost_function(theta0, theta1, X, y))   
-
+   
+    
+if __name__ == "__main__":
+	main();
