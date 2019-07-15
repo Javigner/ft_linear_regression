addedcdfaac84e9d2be57cd168366b3c502e594d
unknown <javigner@student.42.fr>
2019-07-15 02:10:47 +0200

    Denormalization

--- Training.py
+++ Training.py
@@ -8,13 +8,13 @@ def prediction(theta0, theta1, test_value):
 def cost_function_theta0(theta0, theta1, X, y):
     result = 0
     for i in range(len(X)):
-        result += (theta0 + theta1 * X[i] - y[i])
+        result += ((theta0 + theta1 * X[i]) - y[i])
     return result
 
 def cost_function_theta1(theta0, theta1, X, y):
     result = 0
     for i in range(len(X)):
-        result += (theta0 + theta1 * X[i] - y[i]) * X[i]
+        result += ((theta0 + theta1 * X[i]) - y[i]) * X[i]
     return result
 
 def mean_normalization(liste):
@@ -25,8 +25,6 @@ def mean_normalization(liste):
     
 def Gradient_descent(theta0, theta1, X, y):
     learning_rate = 0.1
-    tmptheta0 = float(1)
-    tmptheta1 = float(1)
     tmp0 = float(2)
     tmp1 = float(2)
     X = mean_normalization(X)
@@ -34,11 +32,11 @@ def Gradient_descent(theta0, theta1, X, y):
     while (theta0 != tmp0 and theta1 != tmp1):
         tmp0 = theta0
         tmp1 = theta1
-        tmptheta0 = theta0 - learning_rate * (1 / len(X)) * cost_function_theta0(theta0, theta1, X, y)
-        tmptheta1 = theta1 - learning_rate * (1 / len(X)) * cost_function_theta1(theta0, theta1, X, y)
+        tmptheta0 = theta0 - learning_rate * (1.0 / len(X)) * cost_function_theta0(theta0, theta1, X, y)
+        tmptheta1 = theta1 - learning_rate * (1.0 / len(X)) * cost_function_theta1(theta0, theta1, X, y)
         theta0 = tmptheta0
         theta1 = tmptheta1
-        print(theta0, theta1)
+        
         
     return (theta0, theta1)
     
@@ -49,6 +47,9 @@ def main():
     theta0 = float(0)
     theta1 = float(0)
     theta0, theta1 = Gradient_descent(theta0, theta1, X, y)
+    theta0 = theta0 * max(y);
+    theta1 = theta1 * max(y) / max(X)
+    print(theta0, theta1)
     print(prediction(theta0, theta1, 240000))
     
     
